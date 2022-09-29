edd759e0c90fa818293ce659c8303b50ff66011d
Davidguzman1 <Daviddaguz75@gmail.com>
2022-09-28 20:11:10 -0400

    First commit

---
+++ src/finalProject/Finalclass.java
@@ -0,0 +1,12 @@
+package finalProject;
+
+public class Finalclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("I want a car");
+		
+	}
+
+}
